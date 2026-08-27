cb82330d9802b5186adaf55cc32d5e903bc519ad
Logeshwaran J <logeshwaran.j@mobiasia.com>
2026-08-27 16:08:34 +0530

    chore: initialize GoMovie backend

---
+++ src/main/java/com/gomovie/GomovieApplication.java
@@ -0,0 +1,13 @@
+package com.gomovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomovie/GomovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
